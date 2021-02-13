244ba3fef9d4e200617c1c668a5a5e58f191a2c8
Bison Brandon <brandon@bisontrails.co>
2021-02-12 19:55:06 -0500

    Setup local dev

--- access/slack/init.sh
+++ access/slack/init.sh
@@ -1,14 +1,11 @@
 #/usr/bin/env bash -eo pipefail
 
-cp /var/config/teleport-slack.toml /etc/teleport-slack.toml
+cat secret | jq --raw-output '.["auth.cas"]' | base64 -d > auth.cas
+cat secret | jq --raw-output '.["auth.key"]' | base64 -d > auth.key
+cat secret | jq --raw-output '.["auth.crt"]' | base64 -d > auth.crt
+token="$(cat secret | jq --raw-output '.slack_oauth_token')"
+signkey="$(cat secret | jq --raw-output '.slack_signing_secret')"
 
-cat /tmp/secret | jq --raw-output '.["auth.cas"]' | base64 -d > /var/telebot/auth.cas
-cat /tmp/secret | jq --raw-output '.["auth.key"]' | base64 -d > /var/telebot/auth.key
-cat /tmp/secret | jq --raw-output '.["auth.crt"]' | base64 -d > /var/telebot/auth.crt
-token="$(cat /tmp/secret | jq --raw-output '.slack_oauth_token')"
-signkey="$(cat /tmp/secret | jq --raw-output '.slack_signing_secret')"
+sed -i '' -e "s/{token}/${token}/" config.toml
+sed -i '' -e "s/{secret}/${signkey}/" config.toml
 
-sed -i "s/{token}/${token}/" /etc/teleport-slack.toml
-sed -i "s/{secret}/${signkey}/" /etc/teleport-slack.toml
-
-teleport-slack start
